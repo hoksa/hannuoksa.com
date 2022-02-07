37d093032545deeefbb3a0439f52a90c0477e7bc
Hannu Oksa <hannu@hannuoksa.com>
2022-02-07 22:55:59 +0200

    Animate spinners

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,32 @@
 import anime from "animejs";
 
 const introAnimation = (function () {
-  const spinnerEl = document.querySelectorAll(".anim-spinner");
-
   let introAnimationTimeline = anime
     .timeline({
       loop: false,
       autoplay: false,
     })
     .add({
-      targets: spinnerEl,
-      duration: 0,
+      targets: ".anim-spinner",
+      duration: 1,
       begin: function (anim) {
-        console.log("hello");
+        anim.animatables.forEach(function (a) {
+          console.log(a.target.children.length);
+          anime({
+            targets: a.target.children,
+            opacity: [0.01, 1],
+            duration: 1500,
+            delay: function (el, i, l) {
+              return (500 / l) * i;
+            },
+          });
+        });
       },
+    })
+    .add({
+      targets: "h1",
+      duration: 1000,
+      opacity: [0.001, 1],
     });
 
   return introAnimationTimeline;
