324e048305b9e999a13829d07af395f82cad4ebb
Hannu Oksa <hannu@hannuoksa.com>
2022-02-07 21:55:57 +0200

    Remove unused fonts

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,20 @@
 import anime from "animejs";
 
 const introAnimation = (function () {
-  const sphereEl = document.querySelector(".anim-sphere");
-  const sectionEl = document.querySelectorAll(".anim-section");
+  const spinnerEl = document.querySelectorAll(".anim-spinner");
 
   let introAnimationTimeline = anime
     .timeline({
       loop: false,
       autoplay: false,
-      easing: "easeInOutSine",
-      duration: 500,
     })
-    .add(
-      {
-        targets: sphereEl,
-        clipPath: ["circle(0)", "circle(50%)"],
-        easing: "easeInExpo",
-        duration: 500,
+    .add({
+      targets: spinnerEl,
+      duration: 0,
+      begin: function (anim) {
+        console.log("hello");
       },
-      500
-    )
-    .add(
-      {
-        targets: sphereEl,
-        opacity: 0.6,
-        clipPath: "circle(48%)",
-      },
-      1200
-    )
-    .add(
-      {
-        targets: sectionEl,
-        opacity: [0, 1],
-        easing: "easeOutSine",
-        delay: anime.stagger(150),
-        duration: 750,
-      },
-      "-=100"
-    );
+    });
 
   return introAnimationTimeline;
 })();
