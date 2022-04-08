124084b0ace20d2abfc5cad6275bcc643313b8ef
Hannu Oksa <hannu@hannuoksa.com>
2022-04-08 20:57:51 +0300

    Rewrite page build animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,20 @@
 import anime from "animejs";
 
 const introAnimation = (function () {
-  let introAnimationTimeline = anime
+  const paragraphs = document.querySelectorAll(".anim-para");
+  
+  paragraphs.forEach(function (el) {
+    el.innerHTML = el.innerHTML.replace(/(^|<\/?[^>]+>|\s+)([^\s<]+)/g, '$1<span class="anim-word-outer"><span class="anim-word-inner">$2</span></span>');
+  });
+
+  anime.set(".anim-spinner div", {opacity: 0});
+  anime.set(".anim-border", {scaleX: 0, transformOrigin: "left center"});
+  anime.set(".anim-word-outer", {display: "inline-flex", overflow: "hidden"});
+  anime.set(".anim-word-inner", {display: "inline-flex", translateY: "100%"});
+  anime.set(".anim-list li", {display: "inline-flex", overflow: "hidden"});
+  anime.set(".anim-list li a", {display: "inline-flex", translateY: "100%"});
+  
+  const introAnimationTimeline = anime
     .timeline({
       loop: false,
       autoplay: false,
@@ -9,25 +22,51 @@ const introAnimation = (function () {
     .add({
       targets: ".anim-spinner",
       duration: 1,
+      translateX: 0,
       begin: function (anim) {
         anim.animatables.forEach(function (a) {
-          console.log(a.target.children.length);
           anime({
             targets: a.target.children,
             opacity: [0.01, 1],
-            duration: 1500,
+            duration: 150,
+            easing: "linear",
             delay: function (el, i, l) {
-              return (500 / l) * i;
+              return (a.id * 200) + (600 / l) * i;
             },
           });
         });
       },
-    })
+    }, 1000)
     .add({
-      targets: "h1",
-      duration: 1000,
-      opacity: [0.001, 1],
-    });
+      targets: ".anim-border",
+      duration: 1200,
+      scaleX: 1,
+      delay: anime.stagger(200),
+      easing: "easeOutQuart",
+    }, 1200)
+    .add({
+      targets: paragraphs,
+      duration: 1,
+      translateX: 0,
+      begin: function (anim) {
+        anim.animatables.forEach(function (a) {
+          anime({
+            targets: a.target.querySelectorAll(".anim-word-inner"),
+            duration: 250,
+            translateY: 0,
+            delay: anime.stagger(15, {start: a.id * 200}),
+            easing: "easeOutQuart",
+          });
+        });        
+      },
+    }, 1300)
+    .add({
+      targets: ".anim-list li a",
+      duration: 250,
+      translateY: 0,
+      easing: "easeOutQuart",
+      delay: anime.stagger(50),
+    }, 2500);
 
   return introAnimationTimeline;
 })();
